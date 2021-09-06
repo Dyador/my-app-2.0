599a2e59fa58cb720c4ee7b312310dba9958258a
Drutskiy Volodymyr <dyadorunity@gmail.com>
2021-09-06 20:07:18 +0300

    14

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,19 +3,19 @@ import style from './Header.module.css'
 const Header = () =>{
     return(
      <div className={style.wrapper}>
-       <div className={style.login}>
+       <div className={style.item}>
        <a>login</a>
        </div>
-       <div className={style.login}>
+       <div className={style.item}>
        <a>Автозапчасти</a>
        </div>
-       <div className={style.login}>
+       <div className={style.item}>
        <a>Автотовары</a>
        </div>
-       <div className={style.login}>
+       <div className={style.item}>
        <a>Автомобили</a>
        </div>
-       <div className={style.login}>
+       <div className={style.item}>
        <a>Car Life</a>
        </div>
        
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -3,19 +3,19 @@ import style from './SideBar.module.css'
 const SideBar = () => {
 return(
     <div className={style.wrapper}>
-      <div>
+      <div className={`${style.item} ${style.active}`}>
         <a>Все авто</a>
       </div>
-      <div>
+      <div className={style.item}>
         <a>Бу</a>
       </div>
-      <div>
+      <div className={style.item}>
         <a>Новые</a>
       </div>
-      <div>
+      <div className={style.item}>
         <a>Под пригон</a>
       </div>
-      <div>
+      <div className={style.item}>
         <a>Нерастаможенные</a>
       </div>
     </div>

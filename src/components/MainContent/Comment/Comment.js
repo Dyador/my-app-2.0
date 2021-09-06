47d6edb45deb79f3d9791f985d37fa03981e6bf5
Drutskiy Volodymyr <dyadorunity@gmail.com>
2021-09-06 23:19:37 +0300

    16

---
+++ src/components/MainContent/Comment/Comment.js
@@ -0,0 +1,18 @@
+import style from './Comment.module.css'
+import Review from './Review/Review'
+const Comment = (props) =>{
+    return(
+        <div className={style.wrapper}>
+        <div>
+        <textarea></textarea>
+        <button>Add comment</button>
+        </div>
+        <div>
+        <Review comment='take it'  likeCount='10'/>
+        <Review likeCount='5' />
+        <Review likeCount='1'/>
+        </div>
+        </div>
+    )
+} 
+export default Comment
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,9 +1,11 @@
 import style from './MainContent.module.css'
-
+import Comment from './Comment/Comment'
 const MainContent = () => {
     return(
         <div className={style.wrapper}>
+         <div className={style.mainContent}>
          MainContent
+         </div>
          <div className={style.content}>
          <div >
          <img className={style.img} src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Audi_A8_L_D5_IMG_0066.jpg/1200px-Audi_A8_L_D5_IMG_0066.jpg'/>
@@ -15,6 +17,7 @@ const MainContent = () => {
              <a>Long</a>
              <a>55000$</a>
          </div>
+         <Comment />
          </div>
         </div>
     )

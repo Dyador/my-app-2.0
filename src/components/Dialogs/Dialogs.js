33ce68dc1bb8b9f6080cc2131bcd93de9c0e771f
Drutskiy Volodymyr <dyadorunity@gmail.com>
2021-09-07 17:54:08 +0300

    24

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -7,6 +7,20 @@ import Messages from './Messages'
 
 
 const Dialogs = (props) => {
+ let DialogsItemData = [
+   {name: 'Kristian', id:'1'},
+   {name: 'Jessi', id: '2'},
+   {name: 'David', id: '3'},
+   {name: 'Mayson', id: '4'},
+   {name: 'Skott', id: '5'},
+  ]
+  
+  let MessagesData =[
+    {messages: " I'll call you", id:'1'},
+    {messages: 'Of course ', id: '2'},
+    {messages: "When it's convenient", id: '3'},
+    
+  ]
 return(
     <div className={style.wrapper}>
       
@@ -15,28 +29,28 @@ return(
          
       <div className={style.contacts}>   
          <div>
-           <DialogsItem name='Kristian' id='1'/>
+            <DialogsItem name={DialogsItemData[0].name} id={DialogsItemData[0].id}/>
          </div>
          <div>
-           <DialogsItem name='Jessi' id='2'/>
+           <DialogsItem name={DialogsItemData[1].name} id={DialogsItemData[1].id}/>
          </div>
          <div>
-           <DialogsItem name='David' id='3'/>
+           <DialogsItem name={DialogsItemData[2].name} id={DialogsItemData[2].id}/>
          </div>
          <div>
-           <DialogsItem name='Mayson' id='4'/>
+           <DialogsItem name={DialogsItemData[3].name} id={DialogsItemData[3].id}/>
          </div>
          <div>
-           <DialogsItem name='Skott' id='5'/>
+           <DialogsItem name={DialogsItemData[4].name} id={DialogsItemData[4].id}/>
          </div>
          </div>
 
          
           
       <div className={style.messages}>
-      <Messages messages=" I'll call you" />
-      <Messages messages='Of course ' />
-      <Messages messages="When it's convenient" />
+      <Messages messages={MessagesData[0].messages} />
+      <Messages messages={MessagesData[1].messages} />
+      <Messages messages={MessagesData[2].messages} />
       </div>   
      
     </div>
--- src/components/MainContent/Comment/Comment.js
+++ src/components/MainContent/Comment/Comment.js
@@ -1,6 +1,13 @@
 import style from './Comment.module.css'
 import Review from './Review/Review'
 const Comment = (props) =>{
+   let ReviewData =[
+       {comment: 'take it',  likeCount: '10'},
+       {comment: 'OK',  likeCount: '5'},
+       {comment: '',  likeCount: '1'},
+   ]
+
+
     return(
         <div className={style.wrapper}>
         <div>
@@ -10,9 +17,9 @@ const Comment = (props) =>{
         <button>Add comment</button>
         </div>
         <div>
-        <Review comment='take it'  likeCount='10'/>
-        <Review likeCount='5' />
-        <Review likeCount='1'/>
+        <Review comment={ReviewData[0].comment}  likeCount={ReviewData[0].likeCount}/>
+        <Review comment={ReviewData[1].comment}  likeCount={ReviewData[1].likeCount} />
+        <Review comment={ReviewData[2].comment}  likeCount={ReviewData[2].likeCount}/>
         </div>
         </div>
     )
